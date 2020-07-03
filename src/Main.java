5d2a8544b18d45a1a9e8dc2abc80c63094297865
VictorGarcia23 <garciavictor1111@gmail.com>
2020-07-03 10:49:21 -0400

    calculo area

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println(" Dame el radio: ");
+        int radio = s.nextInt();
+        double area = (radio * radio)  * (Math.PI);
+                System.out.println(area + " metros cuadrados");
+    }
+}
